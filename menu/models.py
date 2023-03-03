46b31f73ee530cf0a0b651c329712a7bcadf66e6
jagernau <stepanenkov25@gmail.com>
2023-03-04 02:11:41 +0300

    версия 2

--- menu/models.py
+++ menu/models.py
@@ -5,4 +5,6 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
