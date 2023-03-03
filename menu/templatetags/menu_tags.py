48686f2a0d6ed860e8fa4f397118af39bea23e87
jagernau <stepanenkov25@gmail.com>
2023-03-03 15:14:34 +0300

    без создания словаря, чисто на queriset

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -2,6 +2,7 @@ from django import template
 from menu.models import MenuItem
 from .drop_down_stule import stule
 from django.utils.safestring import mark_safe
+
 register = template.Library()
 
 
@@ -9,28 +10,34 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
-    current_url: str = context['request'].path
-    menu_name = current_url.replace("/", "")
-    menu_items = MenuItem.objects.all()
-    menu_dict = {}
-    html_code = stule
-    for item in menu_items:
-         menu_dict[item.id] = {
-             'name': item.name,
-             'url': item.url,
-             'parent': item.parent_id,
-             'children': [],
-             'is_active': item.url == current_url
-         }
-         if item.parent_id:
-             menu_dict[item.parent_id]['children'].append(menu_dict[item.id])
-    html_code += '<div class="dropdown">'
-    for i in menu_dict.values():
-        html_code += f'<button class="dropbtn">{i["name"]}</button>'
+    request = context['request']
+    menu_items = MenuItem.objects.filter(name=menu_name).select_related('parent').order_by('id')
+    active_url = request.path_info
 
+    def is_active(item):
+        return item.url == active_url or active_url.startswith(item.url) and len(active_url) > len(item.url)
 
-        html_code += "</div>"
-    html_code += "</div>"
+    def render_menu(menu_items):
+        output = ''
+        for item in menu_items:
+            is_item_active = is_active(item)
+            has_children = item.children.exists()
+            if has_children:
+                submenu = render_menu(item.children.all())
+            else:
+                submenu = ''
+            css_classes = []
+            if is_item_active:
+                css_classes.append('active')
+            if has_children:
+                css_classes.append('has-submenu')
+            output += f'<li class="{ " ".join(css_classes) }">'
+            if item.url:
+                output += f'<a href="{ item.url }"{" class=/"active/"" if is_item_active else ""}>{ item.label }</a>'
+            else:
+                output += f'<span>{ item.label }</span>'
+            output += submenu
+            output += '</li>'
+        return f'<ul>{ output }</ul>'
 
-
-    return mark_safe(html_code)
+    return render_menu(menu_items)
